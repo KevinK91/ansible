7bbaad95a92deceee927ea404cfa9ad46d8ec002
U-ADESSO\Kirst <Kirst@DE-ADN-2H2D7H2.adesso.local>
2017-12-22 14:49:48 +0100

    add replication check postgresql for repmgr and change playbook name

---
+++ roles/icinga-client/files/postgresql/check_repmgr.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+#import modules
+import argparse
+import subprocess
+import sys
+
+def arg_pars():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', action='store', dest='user', default='postgres',
+                    help='To execute the replication commands on localhost\
+                   Default user is postgres')
+    parser.add_argument('-f', action='store', dest='file', default='/etc/repmgr/10/repmgr.conf',
+                    help=' Defines the replication configuration file\
+                   Default is /etc/repmgr/10/repmgr.conf')
+    parser.add_argument('-a', action='store', dest='action', required=True,
+                    help=' Define which kind of check should be done\
+                  role: checks if the node has the expected role\
+                  replication-lag: checks if the node is lagging by more than replication_lag_warning or replication_lag_critical\
+                  archive-ready: checks for WAL files which have not yet been archived\
+                  downstream: checks that the expected downstream nodes are attached\
+                  slots: checks there are no inactive replication slots')
+    return parser.parse_args()
+
+def repmgr_check(args):
+    cmd = '/usr/bin/repmgr node check -f ' + args.file + ' --nagios --' + args.action
+    try: 
+        proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print proc.communicate()[0]
+        if proc.wait() == 6:
+            sys.exit(2)
+        sys.exit(proc.wait())
+    except OSError, error:
+        error = str(error)
+        if error == "No such file or directory":
+            print "UNKNOWN: Cannot find utility '%s" % error
+            sys.exit
+        else:
+            print "UNKNOWN: Error trying to run utility %s" % error
+
+
+def main():
+    repmgr_check(arg_pars()) 
+
+if __name__ == '__main__':
+  main()
