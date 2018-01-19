71f44253d0a737ba3d9cea26bd2357d036984326
Conrad Ansible <ansible@vm-monitoring-oase-prod.c.de-ecom-1505-oase.internal>
2018-01-19 14:33:12 +0000

    final version for bi check

--- roles/icinga-client/files/check_business.py
+++ roles/icinga-client/files/check_business.py
@@ -17,20 +17,29 @@ def arg_pars():
                         Command to list available processes\
                         icingacli businessprocess process list <Name of the config>\
                         ')
-
+    parser.add_argument('--color', dest='color', action='store_true', default=False, 
+                    help='Activate colored output')
     return parser.parse_args()
 
+#This colored output only wirks in the shell and the normal output is colored in Icingaweb2
 def color_output(result):
     result = result.replace("CRITICAL", "\033[1;31;40mCRITICAL\033[0m")
     result = result.replace("WARNING", "\033[1;33;40mWARNING\033[0m")
     result = result.replace("OK", "\033[1;32;40mOK\033[0m")
     return result
 
-def check_business(result):
+def check_business(result, color):
     if re.search('CRITICAL', result) or re.search('WARNING', result) :
-        print(color_output(result))
+	result = result.replace('OK', 'WARNING',1)
+        if color: 
+            print(color_output(result))
+            sys.exit(1)
+        print result
         sys.exit(1)
-    print(color_output(result))
+    if color: 
+        print(color_output(result))
+        sys.exit(0)
+    print result
     sys.exit(0)
 
 if __name__ == '__main__':
@@ -39,8 +48,11 @@ if __name__ == '__main__':
     try:
         proc = subprocess.Popen(cmd.split("#"), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if proc.wait() == 0:
-            check_business(proc.communicate()[0])
-        print(color_output(proc.communicate()[0]))
+            check_business(proc.communicate()[0], args.color)
+        if args.color: 
+            print(proc.communicate()[0])
+            sys.exit(proc.wait())
+        print(proc.communicate()[0])
         sys.exit(proc.wait())		
     except Exception as error:
         error = str(error)
