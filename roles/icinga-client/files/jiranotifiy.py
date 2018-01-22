ec0ad69e5ac61ad5597e94119715d5a6bcf33911
Conrad Ansible <ansible@vm-monitoring-oase-prod.c.de-ecom-1505-oase.internal>
2018-01-22 17:18:04 +0000

    fix some issues

--- roles/icinga-client/files/jiranotifiy.py
+++ roles/icinga-client/files/jiranotifiy.py
@@ -6,6 +6,8 @@ import os
 import ConfigParser
 import requests, json
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import re
 from jira import JIRA
 
@@ -16,9 +18,6 @@ def arg_pars():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', action='store', dest='configfile',  required=True,
                     help='This configuration file contains the Jira connection parameters')
-    parser.add_argument('-i', action='store', dest='issue', 
-                    help='Set the issue to close or modify the issue\
-                    Use the correct id or name')
     parser.add_argument('-p', action='store', dest='project',  required=True,
                     help='Set the project in Jira\
                     Use the correct id or name')
@@ -40,6 +39,9 @@ def arg_pars():
                     help='Set the notification type from icinga2')
     parser.add_argument('-nh', action='store', dest='host', 
                     help='Set the hostname from icinga2')
+    parser.add_argument('-nrt', action='store', dest='requesttype', 
+                    help='Set the custom filed for request type in Jira to see the issue in the portal\
+                     Ask your Jira admin about the internal id')
     parser.add_argument('-nst', action='store', dest='servicestate', 
                     help='Set the service state from icinga2')
     parser.add_argument('-ns', action='store', dest='service', 
@@ -48,10 +50,12 @@ def arg_pars():
                     help='Set notification comment from icinga2')
     parser.add_argument('-no', action='store', dest='organisation', 
                     help='Set the custom field for organisation. \
-                    This field must be created in Jira')
+                    This field must be created in Jira\
+                    Ask your Jira admin about the internal id')
     parser.add_argument('-ne', action='store', dest='environment', 
                     help='Set the custom field for envirionment. \
-                    This field must be created in Jira')
+                    This field must be created in Jira\
+                    Ask your Jira admin about the internal id')
     
 
     return parser.parse_args()
@@ -74,6 +78,11 @@ def open_jira_session(server, username, password, verify=False):
     return JIRA(options={'server': server, 'verify': verify},
                 basic_auth=(username, password))
 
+def color_jira_summary(summary):
+    summary = summary.replace('WARNING', '{color:#f79232}WARNING{color}')
+    summary = summary.replace('OK', '{color:#14892c}OK{color}')
+    summary = summary.replace('CRITICAL', '{color:#d04437}CRITICAL{color}')
+    return summary
 def open_jira_issue(jira, args):
     components = list()
     try:
@@ -92,77 +101,88 @@ def open_jira_issue(jira, args):
     issue_dict = {
         'project': {'key': args.project},
         'summary': args.summary,
-        'description': args.description,
+        'description': color_jira_summary(args.description),
         'issuetype': {'name': args.issuetype},
-	'priority': {'name': prio},
+        'priority': {'name': prio},
         'components': components,
-        'customfield_10003': args.organisation,
-        'customfield_10500': args.environment,
-    }
-    issue = jira.create_issue(fields=issue_dict)
+        'customfield_10502': { 'value': args.environment},
+        }
+    try:
+         issue = jira.create_issue(fields=issue_dict)
+    except Exception as error:
+        print error
     return issue
 
+#update jira issue for organisation and requesttype does not work at the moment
+def update_jira_issue(jira, issue, args):
+    issue_fields = {'customfield_10003': args.organisation,
+              'customfield_10002': args.requesttype,
+             }
+    try:
+        issue.update(fields=issue_fields)
+    except Exception as error:
+        print error
+
+
 def comment_jira_issue(jira, args, config):
-	pattern = re.compile("^(\w+)-(\d+)$")
-	headers = {'Accept': 'application/json', 'X-HTTP-Method-Override': 'GET'}
-	data = {'type': 'Service', 'filter': "host.name == filterHost && service.name == filterService", "filter_vars": { "filterHost": args.host, "filterService": args.service}}
-	r = requests.post(config['icinga2_url'] + "objects/comments", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
-	if (r.status_code == 200):
-		for ele in  r.json()['results']:
-			#print json.dumps(ele, sort_keys=True, indent=4, separators=(',', ': '))
-			if pattern.match(ele['attrs']['text']):
-				jiracomment = jira.add_comment(ele['attrs']['text'], args.summary)    # no Issue object required
-				return  ele['attrs']['name']
-	else:
-		print r.text
-		r.raise_for_status()
+    pattern = re.compile("^(\w+)-(\d+)$")
+    headers = {'Accept': 'application/json', 'X-HTTP-Method-Override': 'GET'}
+    data = {'type': 'Service', 'filter': "host.name == filterHost && service.name == filterService", "filter_vars": { "filterHost": args.host, "filterService": args.service}}
+    r = requests.post(config['icinga2_url'] + "objects/comments", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
+    if (r.status_code == 200):
+        for ele in  r.json()['results']:
+            if pattern.match(ele['attrs']['text']):
+                jiracomment = jira.add_comment(ele['attrs']['text'], args.summary)    # no Issue object required
+                return  ele['attrs']['name']
+    else:
+        print r.text
+        r.raise_for_status()
 
 def icinta2_remove_comment(config, args, comment_name):
-	headers = {
+    headers = {
     'Accept': 'application/json',}
-	commentmatch = args.host + "!" + args.service + "!" + comment_name
-	data = {"comment": commentmatch }
-	r = requests.post(config['icinga2_url'] + "actions/remove-comment", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
+    commentmatch = args.host + "!" + args.service + "!" + comment_name
+    data = {"comment": commentmatch }
+    r = requests.post(config['icinga2_url'] + "actions/remove-comment", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
 
 def icinga2_service_add_comment(config, args, comment):
-	headers = {
+    headers = {
     'Accept': 'application/json',}
-	data = {'type': 'Service', 'filter': "host.name == filterHost && service.name == filterService", "filter_vars": { "filterHost": args.host, "filterService": args.service}, "author": config['icinga2_author'], "comment": comment}
-	r = requests.post(config['icinga2_url'] + "actions/add-comment", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
+    data = {'type': 'Service', 'filter': "host.name == filterHost && service.name == filterService", "filter_vars": { "filterHost": args.host, "filterService": args.service}, "author": config['icinga2_author'], "comment": comment}
+    r = requests.post(config['icinga2_url'] + "actions/add-comment", headers=headers, json=data, verify=False, auth=(config['icinga2_apiuser'], config['icinga2_apipassword']))
 
 if __name__ == '__main__':
-	args = arg_pars()
-	try:
-		config = read_configuration_file(args.configfile)
-	except IOError as e:
-		print("Could not find configuration file: %s" % e)
-		sys.exit(2)
-	except ValueError as e:
-		print(e)
-		sys.exit(2)
-	except ConfigParser.NoSectionError as e:
-		print("Configuration file is corrupt: %s" % e)
-		sys.exit(2)
+    args = arg_pars()
+    try:
+        config = read_configuration_file(args.configfile)
+    except IOError as e:
+        print("Could not find configuration file: %s" % e)
+        sys.exit(2)
+    except ValueError as e:
+        print(e)
+        sys.exit(2)
+    except ConfigParser.NoSectionError as e:
+        print("Configuration file is corrupt: %s" % e)
+        sys.exit(2)
 
-	try:
-		jira = open_jira_session(config['jira_url'], config['jira_username'], config['jira_password'])
-	except Exception as e:
-		print e
-		sys.exit(2)
-	if args.notifytype.lower() == "problem":
-		try:
-			new_issue = open_jira_issue(jira, args) 
-			#icinga2_service_add_comment(config['icinga2_url'], args.host, args.service, config['icinga2_apiuser'], config['icinga2_apipassword'], config['icinga2_author'], str(new_issue) + " " + args.comment)
-			icinga2_service_add_comment(config, args, str(new_issue)) 
-		except Exception as e:
-			print "There is a exception in creating jira issue and comment issue number in icinga2\n", e
-			sys.exit(2)
-		
-	elif args.notifytype.lower() == "recovery":
-		try:
-			comment_name = comment_jira_issue(jira, args, config)
-			icinta2_remove_comment(config, args, comment_name) 				
-		except Exception as e:
-			print "There is a exception in creating jira comment and removing comment in icinga2\n", e
-			sys.exit(2)
+    try:
+        jira = open_jira_session(config['jira_url'], config['jira_username'], config['jira_password'])
+    except Exception as e:
+        print e
+        sys.exit(2)
+    if args.notifytype.lower() == "problem":
+        try:
+            new_issue = open_jira_issue(jira, args)
+            icinga2_service_add_comment(config, args, str(new_issue)) 
+        except Exception as e:
+            print "There is a exception in creating jira issue and comment issue number in icinga2\n", e
+            sys.exit(2)
+        
+    elif args.notifytype.lower() == "recovery":
+        try:
+            comment_name = comment_jira_issue(jira, args, config)
+            icinta2_remove_comment(config, args, comment_name)              
+        except Exception as e:
+            print "There is a exception in creating jira comment and removing comment in icinga2\n", e
+            sys.exit(2)
 
